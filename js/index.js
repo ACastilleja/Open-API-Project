49c1fc525d588b1ef2316b27f6ecdebff3c8942b
ACastilleja <castillejaarturo@gmail.com>
2026-02-25 17:27:57 -0500

    added js api fetch

--- js/index.js
+++ js/index.js
@@ -19,3 +19,16 @@ footer.appendChild(copyright);
 document.body.appendChild(footer);
 
 // Weather API
+async function getWeather(){
+    try{
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=27.9658&longitude=-82.8001&daily=uv_index_max,wind_gusts_10m_max,temperature_2m_max,temperature_2m_min,precipitation_probability_max,precipitation_hours&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,apparent_temperature,soil_temperature_6cm,cloud_cover&current=temperature_2m,rain,wind_gusts_10m&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch");
+        if(!response.ok){
+            throw new Error (`Error: ${response.status}`);
+        }
+        const weatherData = await response.json();
+        console.log(weatherData);
+    }catch(error){
+    console.error(error);
+}
+};
+getWeather();
