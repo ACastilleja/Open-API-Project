f5b1f74117f5f86329a73619565150140b3ba7f4
ACastilleja <castillejaarturo@gmail.com>
2026-03-04 22:15:13 -0500

    added DOM  to add  temp data to HTML and give error

--- js/index.js
+++ js/index.js
@@ -19,16 +19,94 @@ footer.appendChild(copyright);
 document.body.appendChild(footer);
 
 // Weather API
-async function getWeather(){
-    try{
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=27.7253&longitude=-82.7412&daily=uv_index_max&hourly=temperature_2m,wind_gusts_10m,cloud_cover,precipitation_probability&current=temperature_2m,apparent_temperature,rain,wind_gusts_10m&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch");
-        if(!response.ok){
-            throw new Error (`Error: ${response.status}`);
+const searchBtn = document.getElementById('search-btn');
+const beachInput = document.getElementById('beach-name');
+// const displayWeather = document.getElementById('localWeather');
+const weatherContainer = document.querySelector('.localWeather');
+
+searchBtn.addEventListener('click', async ()=> {
+
+    const beachName = beachInput.value;
+    if (!beachName) {
+        weatherContainer.textContent = "Please type in a beach name."
+        return;
+        
+    }
+    
+    try {
+        const geoRes = await fetch (`https://geocoding-api.open-meteo.com/v1/search?name=${beachName}&count=1&language=en&format=json&countryCode=US`);
+        const geoData = await geoRes.json();
+        console.log(geoData);
+
+        if(!geoData.results){
+            weatherContainer.textContent = "Beach not found please try again";
+            return;
         }
-        const weatherData = await response.json();
+
+        const {latitude, longitude, name, admin1 } = geoData.results[0];
+        console.log(latitude, longitude, name, admin1);
+
+        const weatherRes = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=precipitation_probability,temperature_2m,uv_index,wind_gusts_10m,cloud_cover&current=temperature_2m,apparent_temperature,wind_gusts_10m,cloud_cover,showers&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`);
+        const weatherData = await weatherRes.json();
         console.log(weatherData);
-    }catch(error){
-    console.error(error);
-}
-};
-getWeather();
+
+        const temp = weatherData.current.temperature_2m;
+        console.log(`Temperature: ${temp}F`);
+        const realFeel = weatherData.current.apparent_temperature;
+        console.log(`Real Feel: ${realFeel}F`);
+        const currentPrecip = weatherData.hourly.precipitation_probability[0];
+        console.log(`Rain Chance: ${currentPrecip}%`);
+        const gust = weatherData.current.wind_gusts_10m;
+        console.log(`Wind Gust: ${gust}mph`);
+        const currentUV = weatherData.hourly.uv_index[0];
+        console.log(`UV Index: ${currentUV}`);
+
+        //const weatherContainer = document.querySelector('.localWeather');
+        const weatherList = document.createElement('ul');
+
+        const displayData = [
+            `Location: ${name}, ${admin1}`,
+            `Temperatur: ${temp}°F`,
+            `Real Feel: ${realFeel}°F`,
+            `Wind Gust: ${gust}mph`,
+            `Rain Probability: ${currentPrecip}%`,
+            `UV Index: ${currentUV}`
+
+        ];
+        displayData.forEach ( text =>{
+            const li = document.createElement('li');
+            li.textContent = text;
+            weatherList.appendChild(li);
+        });
+
+        weatherContainer.textContent = '';
+        weatherContainer.appendChild(weatherList);
+
+
+    } 
+    
+    
+    
+    catch (error){
+        console.log("Error",error);
+        weatherContainer.textContent = "Unable to loach weather";
+        
+    }
+
+
+});
+
+// async function getWeather(){
+//     try{
+//         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=27.7253&longitude=-82.7412&daily=uv_index_max,weather_code&current=temperature_2m,wind_gusts_10m,precipitation&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch");
+//         if(!response.ok){
+//             throw new Error (`Error: ${response.status}`);
+//         }
+//         const weatherData = await response.json();
+//         console.log(weatherData);
+//     }catch(error){
+//     console.error(error);
+// }
+// };
+
+//getWeather();
