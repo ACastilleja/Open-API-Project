81485700119433c14098e2861fc7199521339eb4
ACastilleja <castillejaarturo@gmail.com>
2026-03-07 22:54:33 -0500

    seperated API fetch for current and forcast buttons, also removed extra uv index api

--- js/index.js
+++ js/index.js
@@ -20,12 +20,14 @@ document.body.appendChild(footer);
 
 // Getting html elements for DOM
 const searchBtn = document.getElementById('search-btn');
+const forecastBtn = document.getElementById('forecast-btn');
 const beachInput = document.getElementById('beach-name');
 const weatherContainer = document.querySelector('.localWeather');
 
-//Creating Eventlistener for searchBtn for Beach input
-searchBtn.addEventListener('click', async ()=> {
+//Geo cordinates finder
+let currentCoord = null;
 
+async function getCoord() {
     const beachName = beachInput.value;
     if (!beachName) {
         weatherContainer.textContent = "Please type in a beach name."
@@ -33,7 +35,7 @@ searchBtn.addEventListener('click', async ()=> {
         
     }
     
-    try {
+    try{
         // Geolocation API
         const geoRes = await fetch (`https://geocoding-api.open-meteo.com/v1/search?name=${beachName}&count=1&language=en&format=json&countryCode=US`);
         const geoData = await geoRes.json();
@@ -45,17 +47,38 @@ searchBtn.addEventListener('click', async ()=> {
         }
 
         //geo API json data variable
-        const {latitude, longitude, name, admin1 } = geoData.results[0];
-        console.log(latitude, longitude, name, admin1);
+        
+    return currentCoord = {
+            latitude: geoData.results[0].latitude,
+            longitude: geoData.results[0].longitude,
+            name: geoData.results[0].name,
+            state: geoData.results[0].admin1
+        };
+        
+    }catch (error){
+        console.log("Error",error);
+        weatherContainer.textContent = "Unable to find Beach";
+    }
+};
+
+
+
+//Creating Eventlistener for searchBtn for current weather
+searchBtn.addEventListener('click', async ()=> {
+
+        //Getting Geocoordinates with helper function
+        const location = await getCoord();
+        if(!location){
+            weatherContainer.textContent = "Beach not found please try again";
+            return;
+        }
+        const {latitude,longitude,name,state}=currentCoord;
+        try{
         // Current Weather API
-        const weatherRes = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=precipitation_probability,temperature_2m,apparent_temperature,wind_gusts_10m,cloud_cover&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`);
+        const weatherRes = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=precipitation_probability,temperature_2m,apparent_temperature,wind_gusts_10m,cloud_cover,uv_index&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`);
         const weatherData = await weatherRes.json();
         console.log(weatherData);
-        // Current UV index API
-        const uvRes = await fetch (`https://currentuvindex.com/api/v1/uvi?latitude=${latitude}&longitude=${longitude}`);
-        const uvIndex = await uvRes.json();
-        console.log(uvIndex);
-
+        
         //Weather variables
         const temp = weatherData.current.temperature_2m;
         console.log(`Temperature: ${temp}F`);
@@ -67,7 +90,7 @@ searchBtn.addEventListener('click', async ()=> {
         console.log(`Wind Gust: ${gust}mph`);
         const cloudCover = weatherData.current.cloud_cover;
         console.log(`Cloud Cover= ${cloudCover}`);
-        const currentUV = uvIndex.now.uvi;
+        const currentUV = weatherData.current.uv_index;
         console.log(`UV Index: ${currentUV}`);
         
         
@@ -76,8 +99,8 @@ searchBtn.addEventListener('click', async ()=> {
     
         //Creating data array to use with for Each loop
         const displayData = [
-            `Location: ${name}, ${admin1}`,
-            `Temperatur: ${temp}°F`,
+            `Location: ${name}, ${state}`,
+            `Temperature: ${temp}°F`,
             `Real Feel: ${realFeel}°F`,
             `Wind Gust: ${gust}mph`,
             `Rain Probability: ${currentPrecip}%`,
@@ -104,7 +127,82 @@ searchBtn.addEventListener('click', async ()=> {
     }
 
 
-});//end of searchBtn eventlistener
+});//end of searchBtn current weather eventlistener
+
+//Eventlistener of forcastBtn for 5hr forcasteweather
+forecastBtn.addEventListener('click', async ()=> {
+
+        //Getting Geocoordinates with helper function
+        const location = await getCoord();
+        if(!location){
+            weatherContainer.textContent = "Beach not found please try again";
+            return;
+        }
+        const {latitude,longitude,name,state}=currentCoord;
+        try{
+        // Current Weather API
+        const weatherRes = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=precipitation_probability,temperature_2m,apparent_temperature,wind_gusts_10m,cloud_cover,uv_index&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`);
+        const weatherData = await weatherRes.json();
+        console.log(weatherData);
+        
+        
+        //Creating ul list element
+        const weatherList = document.createElement('ul');
+        const currentHour = new Date().getHours();
+        //looping through the next 5-hrs forecast
+        for (let i=currentHour; i<currentHour+5; i++){
+            const time = new Date(weatherData.hourly.time[i]).toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'});
+
+            //Weather variables
+            console.log(`Time: ${time}`);
+            const temp = weatherData.hourly.temperature_2m[i];
+            console.log(`Temperature: ${temp}F`);
+            const realFeel = weatherData.hourly.apparent_temperature[i];
+            console.log(`Real Feel: ${realFeel}F`);
+            const hourlyPrecip = weatherData.hourly.precipitation_probability[i];
+            console.log(`Rain Chance: ${hourlyPrecip}%`);
+            const gust = weatherData.hourly.wind_gusts_10m[i];
+            console.log(`Wind Gust: ${gust}mph`);
+            const cloudCover = weatherData.hourly.cloud_cover[i];
+            console.log(`Cloud Cover= ${cloudCover}`);
+            const hourlyUV = weatherData.hourly.uv_index[i];
+            console.log(`UV Index: ${hourlyUV}`);
+            
+        
+        
+    
+            //Creating data array to use with for Each loop
+            const displayData = [
+                `Location: ${name}, ${state}`,
+                `Time: ${time}`,
+                `Temperature: ${temp}°F`,
+                `Real Feel: ${realFeel}°F`,
+                `Wind Gust: ${gust}mph`,
+                `Rain Probability: ${hourlyPrecip}%`,
+                `Cloud Cover: ${cloudCover}%`,
+                `UV Index: ${hourlyUV}`
+                
+            ];
+            // for Each loop to create li element
+            displayData.forEach ( text =>{
+                const li = document.createElement('li');
+                li.textContent = text;
+                weatherList.appendChild(li);
+            });
+
+            //appending Weatherlist li list to HTML and adding a class for css
+            weatherContainer.textContent = '';
+            weatherContainer.appendChild(weatherList);
+            weatherContainer.classList.add("weatherCard"); 
+        }
+    } catch (error){
+        console.log("Error",error);
+        weatherContainer.textContent = "Something went wrong. Unable to load weather";
+        
+    }
+
+
+});//end of forcastBtn eventlistener
 
 
 
@@ -112,19 +210,3 @@ searchBtn.addEventListener('click', async ()=> {
 
 
 
-
-// OLD API command
-// async function getWeather(){
-//     try{
-//         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=27.7253&longitude=-82.7412&daily=uv_index_max,weather_code&current=temperature_2m,wind_gusts_10m,precipitation&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch");
-//         if(!response.ok){
-//             throw new Error (`Error: ${response.status}`);
-//         }
-//         const weatherData = await response.json();
-//         console.log(weatherData);
-//     }catch(error){
-//     console.error(error);
-// }
-// };
-
-//getWeather();
